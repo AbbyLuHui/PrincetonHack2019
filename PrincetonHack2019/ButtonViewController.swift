34189e112ab78bec497f5c07dfde9f1d890c085c
Yiyang Zhao <yiyang.zhao@columbia.edu>
2019-11-10 09:12:49 -0500

    Added helderly title

--- PrincetonHack2019/ButtonViewController.swift
+++ PrincetonHack2019/ButtonViewController.swift
@@ -8,30 +8,40 @@
 
 import UIKit
 
+// var buttonPressed = true
+
 class ButtonViewController: UIViewController {
 
     @IBOutlet weak var longButton: UIButton!
     var longGesture = UILongPressGestureRecognizer()
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         longGesture = UILongPressGestureRecognizer(target: self, action: #selector(ButtonViewController.longPress(_:)))
         longButton.addGestureRecognizer(longGesture)
+        
+/*
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.longPress(_:)), userInfo: nil, repeats: true)
+        timer.tolerance = 10
+ */
     }
     
     @IBAction func longPress(_ sender: UILongPressGestureRecognizer) {
+        
         if sender.state == .began
         {
-            
-            // alert
+            // notification
             let alertC = UIAlertController(title:nil, message: "Emergency action cancelled", preferredStyle: .alert)
             // also back to main
             let ok = UIAlertAction(title: "OK", style: .default, handler: {action in self.GoTo()})
             alertC.addAction(ok)
             self.present(alertC, animated: true, completion: nil)
         }
+/*
+        else {
+            buttonPressed = false
+        }
+ */
     }
     
     func GoTo(){
